67318fc5b95a44ac38ee50d034091835d5c6052e
fabiojorge-a22303085 <101054204+fabiojorge-a22303085@users.noreply.github.com>
2025-01-04 23:07:09 +0000

    upd

--- components/ProductCard/ProductCard.tsx
+++ components/ProductCard/ProductCard.tsx
@@ -4,7 +4,7 @@ import { Product } from '@/models/interfaces'
 import React from 'react'
 
 export default function ProductCard({ product }: { product: Product }) {
-  const { title, category, price, image } = product
+  const { id, title, category, price, image } = product
 
   return (
     <article className="m-8 p-8 bg-purple-500 hover:bg-purple-600">
@@ -12,13 +12,10 @@ export default function ProductCard({ product }: { product: Product }) {
       <p>{category}</p>
       <p>${price.toFixed(2)}</p>
       <Image
-  src={product.image}
-  alt={product.title}
-  width={200}
-  height={200}
-  className="rounded-md mb-4 object-cover"
-/>
-
+                        src={product.image}
+                        alt={product.title}
+                        className="w-16 h-16 rounded-md object-cover"
+                      />
     </article>
   )
 }
